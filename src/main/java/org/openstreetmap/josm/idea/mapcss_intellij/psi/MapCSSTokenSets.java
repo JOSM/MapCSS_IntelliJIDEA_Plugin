16912f1496f73b0eb86c48f2b6087adb53e3640e
Taylor Smock <tsmock@meta.com>
2024-08-26 11:42:33 -0600

    elemstyles working
    Signed-off-by: Taylor Smock <tsmock@meta.com>

--- src/main/java/org/openstreetmap/josm/idea/mapcss_intellij/psi/MapCSSTokenSets.java
+++ src/main/java/org/openstreetmap/josm/idea/mapcss_intellij/psi/MapCSSTokenSets.java
@@ -3,6 +3,6 @@ package org.openstreetmap.josm.idea.mapcss_intellij.psi;
 import com.intellij.psi.tree.TokenSet;
 
 public interface MapCSSTokenSets {
-    TokenSet COMMENTS = TokenSet.create(MapCSSTypes.PP_COMMENT_START); // FIXME add END
-    TokenSet IDENTIFIERS = TokenSet.create(MapCSSTypes.KEY);
+    TokenSet COMMENTS = TokenSet.create(MapCSSTypes.BLOCK_COMMENT);
+    TokenSet IDENTIFIERS = TokenSet.create(MapCSSTypes.IDENT);
 }
